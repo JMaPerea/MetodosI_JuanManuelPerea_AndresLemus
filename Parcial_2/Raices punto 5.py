24fdace7929f71bd77ce54ae7af36597659f8f70
JMaPerea <juanma.perea2002@gmail.com>
2023-09-28 11:50:25 -0500

    raices 5

---
+++ Parcial_2/Raices punto 5.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 12:37:33 2023
+
+@author: ADMIN
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sympy as sym
+import math
+
+x = sym.Symbol('x',real=True)
+y = sym.Symbol('y',real=True)
+
+def ObtLaguerreRodrigues(n,x):
+    sys=sym.exp(-x)*x**n
+    if n==0:
+        poly=1
+    if n==1:
+        poly=-x+1
+    else:
+        sys=sym.exp(-x)*x**n
+        poly=((sym.exp(x))/(sym.factorial(n)))*sym.diff(sys,x,n)
+    
+        
+            
+    return poly
+
+def GetDLaguerre(n,x):
+    Pn = ObtLaguerreRodrigues(n,x)
+    return sym.diff(Pn,x,1)
+
+
+
+
+
+def ObtPolinomiosRaices(n,x):
+    xn = np.linspace(0,n+((n-1)*np.sqrt(n)),100)
+    polinomios=[]
+    
+    roots=[]
+    for i in range(n+1):
+        polinomios.append(ObtLaguerreRodrigues(i, x))
+        
+    i=0
+    for k in polinomios:
+        if k==1:
+            root=None
+        else:
+            root=sym.nroots(k)
+            
+        roots.append(root)
+        
+        print(f'Grado {i} Raices {root}')
+        i+=1
+    return roots
+     
+
+ObtPolinomiosRaices(20, x)
+
