ae5f7012fb8280272f24b3de48fda67687d4e76c
JMaPerea <juanma.perea2002@gmail.com>
2023-11-06 15:28:00 -0500

    lunes

--- Parcial_3/Lineal_16.py
+++ Parcial_3/Lineal_16.py
@@ -31,14 +31,14 @@ gamma3 = sp.Matrix([[0, 0, 1, 0],
 gamma=[gamma0,gamma1,gamma2,gamma3]
 
 
-eta = sp.Matrix([[1, 0, 0, 0],
+eta = sp.Matrix([[+1, 0, 0, 0],
                 [0, -1, 0, 0],
                 [0, 0, -1, 0],
                 [0, 0, 0, -1]])
 
 
 
-Valor = sp.Matrix.zeros(4, 4)
+zero = sp.Matrix.zeros(4, 4)
 
 I = sp.eye(4)
 
@@ -48,16 +48,16 @@ for u in range(4):
         anticomm = (gamma[u] * gamma[v]) + (gamma[v] * gamma[u])
 
 
-        Valor += anticomm * eta
+        
         #print(anticomm)
 
-
-
-        expected = 2 * eta * I
-        #print('real',anticomm)
-        #print('expect',expected)
-        print(f"Para: µ={u+1}   v={v+1}")
-        if Valor == expected:
-            print(f"La relación de anticonmutación se verifica y es igual a 2η^µνI=2^{u+1}{v+1}*I.")
-        else:
-            print("La relación de anticonmutación no se verifica.")
+        if u != v:
+        
+            expected = 2 * eta[u,v] * I
+            #print('real',anticomm)
+            #print('expect',expected)
+            print(f"Para: µ={u+1}   v={v+1}")
+            if anticomm == expected:
+                print(f"La relación de anticonmutación se verifica y es igual a 2η^µνI=2^{u+1}{v+1}*I.")
+            else:
+                print("La relación de anticonmutación no se verifica.")
